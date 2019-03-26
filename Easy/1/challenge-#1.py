aa32e69918c29eb24e07d10286b79d318c042e94
Wesley Guthrie <wesleyeguthrie@gmail.com>
2019-03-25 23:47:12 -0400

    initial commit

---
+++ Easy/1/challenge-#1.py
@@ -0,0 +1,16 @@
+def main():
+    name = raw_input("What is your name?")
+    age = raw_input("How old are you?")
+    reddit_username = raw_input("What is your reddit username?")
+    save_to_file(name, age, reddit_username)
+    print("your name is {}, you are {} years old, and your username is {}".format(name, age, reddit_username))
+
+
+def save_to_file(name, age, username):
+    file = open("./user_information.txt", "w")
+    file.write(name + "\n" + age + "\n" + username)
+    file.close()
+
+
+if __name__ == '__main__':
+    main()
